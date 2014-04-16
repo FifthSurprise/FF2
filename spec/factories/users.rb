44256a6fe4f000771795204873dce5a00fea1e9f
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-16 16:12:42 -0400

    upgraded to postgres and made tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@
 FactoryGirl.define do
   factory :user do
     username "John Doe"
+    email "test@test.com"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,16 @@ require 'spec_helper'
 
 describe User do
   # pending "add some examples to (or delete) #{__FILE__}"
-  it 'has a name' do
-    user = create(:user)
-    expect(user.username).to eq ("John Doe")
+  describe 'attributes' do
+    before (:each) do
+      @user = create(:user)
+    end
+    it 'has a name' do
+
+      expect(@user.username).to eq ("John Doe")
+    end
+    it 'has an email' do
+      expect(@user.email).to eq ("test@test.com")
+    end
   end
 end
