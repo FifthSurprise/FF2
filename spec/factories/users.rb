a6e2501bca8931d3906015d4a30a2cd982423e6f
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-16 16:06:29 -0400

    initialized users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@
 
 FactoryGirl.define do
   factory :user do
+    username "John Doe"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a name' do
+    user = create(:user)
+    expect(user.username).to eq ("John Doe")
+  end
 end
