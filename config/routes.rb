5534a91ccd1f2e8982ec8f8d35cdb059b0c7bedc
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-21 23:36:48 -0400

    fixed css issue that affected gravatar img urls.  May affect recall markdown images...

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   
   get '/recalls/:id' => 'recalls#show', :as => :recall
   get '/recalls/:id/processQ' => 'recalls#process', :as => :processQ
-
+a
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
